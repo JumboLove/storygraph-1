575b2725668703b80c2aa03f05d5e61eabd47e70
David Witt <midknight55@gmail.com>
2018-03-25 16:39:02 -0500

    Ditched AirBnB styles for standard styles

---
+++ src/components/StoryGraph.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Story Graph</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StoryGraph'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <story-graph></story-graph>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from '@/components/HelloWorld.vue'
+import StoryGraph from '@/components/StoryGraph.vue'
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
-  },
-};
+    StoryGraph
+  }
+}
 </script>
