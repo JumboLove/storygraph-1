ede258de71e3ec427a212e6672ab071beb116963
David Witt <midknight55@gmail.com>
2018-03-25 16:47:27 -0500

    Setup for StoryGraph prototype work

--- src/components/StoryGraph.vue
+++ src/components/StoryGraph.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
     <h1>Story Graph</h1>
+    <div id="drawing"></div>
   </div>
 </template>
 
 <script>
+import * as SVG from 'svg.js'
+import '@/packages/svg.path.js'
+
 export default {
-  name: 'StoryGraph'
+  name: 'StoryGraph',
+  data () {
+    return {
+      draw: null
+    }
+  },
+  mounted () {
+    // testing
+    this.draw = SVG('drawing').size(300, 300)
+    this.draw.rect(100, 100).attr({fill: '#f06'})
+    // var path = this.draw.path('M100,250 C263,258 174,146 327,155')
+    var path = this.draw.path().M({x: 100, y: 100}).c({x: 0, y: 100}, {x: 100, y: 100}, {x: 100, y: 0})
+    path.fill('none')
+    path.stroke({ color: '#f06', width: 7, linecap: 'round', linejoin: 'round' })
+  }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
     <story-graph></story-graph>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import StoryGraph from '@/components/StoryGraph.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
     StoryGraph
   }
 }
