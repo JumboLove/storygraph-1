b045b13534142058b21747141991ccc479f64c40
David Witt <midknight55@gmail.com>
2018-04-11 17:48:55 -0500

    Added basic functions and debug graph

--- src/components/StoryGraph.vue
+++ src/components/StoryGraph.vue
@@ -13,21 +13,63 @@ export default {
   name: 'StoryGraph',
   data () {
     return {
-      draw: null
+      draw: null,
+      main: null,
+      baseline: 300
     }
   },
   mounted () {
-    // testing
-    this.draw = SVG('drawing').size(300, 300)
-    this.draw.rect(100, 100).attr({fill: '#f06'})
-    // var path = this.draw.path('M100,250 C263,258 174,146 327,155')
-    var path = this.draw.path().M({x: 100, y: 100}).c({x: 0, y: 100}, {x: 100, y: 100}, {x: 100, y: 0})
-    path.fill('none')
-    path.stroke({ color: '#f06', width: 7, linecap: 'round', linejoin: 'round' })
+    this.drawCanvas()
+    this.drawDebugGrid()
+    this.drawMainLine()
+    this.drawSplit(100)
+  },
+  created () {
+
+  },
+  methods: {
+    drawCanvas () {
+      this.draw = SVG('drawing')
+      this.main = this.draw.group()
+    },
+    drawMainLine () {
+      let mainLine = this.main.path().M({x: 0, y: this.baseline}).l({x: 1000, y: 0})
+      mainLine.fill('none')
+      mainLine.stroke({ color: 'black', width: 4, linecap: 'round', linejoin: 'round' })
+    },
+    drawSplit (xPos) {
+      let splitLine = this.main.path().M({x: xPos, y: this.baseline}).c(
+        {x: 50, y: 0},
+        {x: 50, y: -100},
+        {x: 100, y: -100})
+      splitLine.fill('none')
+      splitLine.stroke({ color: 'green', width: 4, linecap: 'round', linejoin: 'round' })
+    },
+    drawDebugGrid () {
+      let pattern = this.draw.pattern(25, 25, function(add) {
+        add.line(0, 0, 0, 25).stroke({width: 1, color: '#ccc'})
+        add.line(0, 0, 25, 0).stroke({width: 1, color: '#ccc'})
+      })
+
+      this.main.rect('100%', '100%').fill(pattern)
+    }
   }
 }
 </script>
 
 <style scoped>
+  #drawing {
+    outline: 1px solid #ccc;
+    height: 400px;
+  }
 
+  #drawing svg {
+    width: 100%;
+    height: 100%;
+  }
+
+  #drawing svg  g {
+    width: 100%;
+    height: 100%;
+  }
 </style>
